d7bde5e1225e6c704adda5b80ef6884710f935b0
belbazanas <belbazanas@gmail.com>
2023-07-21 02:37:02 +0200

    check isConnected for trades

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,8 +2,13 @@
 import { Card } from "../../components/ui/card";
 // components
 import { Navbar, MintFaucet, Buy, Sell, Post, Pairs } from "../../components";
+import { useAccount } from "wagmi";
+import { Button } from "../../components/ui/button";
+import { useWeb3Modal } from "@web3modal/react";
 
 const Home = () => {
+    const { isConnected } = useAccount();
+    const { open } = useWeb3Modal();
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-4">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm flex-col">
@@ -13,20 +18,26 @@ const Home = () => {
                     <MintFaucet />
                 </Card>
                 <Card className="mx-auto mt-10 p-10">
-                    <div className="flex flex-col space-y-4">
-                        <div className="flex flex-col items-center justify-center p-4 w-full">
-                            <Pairs />
+                    {isConnected && (
+                        <div className="flex flex-col space-y-4">
+                            <div className="flex flex-col items-center justify-center p-4 w-full">
+                                <Pairs />
+                            </div>
+                            <div className="p-4">
+                                <Buy />
+                            </div>
+                            <div className="p-4">
+                                <Sell />
+                            </div>
+                            <div className="p-4">
+                                <Post />
+                            </div>
                         </div>
-                        <div className="p-4">
-                            <Buy />
-                        </div>
-                        <div className="p-4">
-                            <Sell />
-                        </div>
-                        <div className="p-4">
-                            <Post />
-                        </div>
-                    </div>
+                    )}
+
+                    {!isConnected && (
+                        <Button onClick={open}> Connect wallet </Button>
+                    )}
                 </Card>
             </div>
         </main>
