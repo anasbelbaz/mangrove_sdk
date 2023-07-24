31db2baf1c15c0a8111a9c6a6a17e15f9e10a506
belbazanas <belbazanas@gmail.com>
2023-07-24 15:20:15 +0200

    fix local developpement

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
     plugins: [react()],
+    define: isProduction ? {} : { "process.env": {} },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
